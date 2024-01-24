bcfea187552f7c3b47f25bf22bb48758daa59fc0
rapidAcceleration-4593 <78278915+rapidAcceleration-4593@users.noreply.github.com>
2024-01-23 18:46:07 -0700

    Updating motor values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,7 +20,7 @@ import swervelib.parser.PIDFConfig;
 public final class Constants
 {
 
-  public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
+  public static final double ROBOT_MASS = (90) * 0.453592; // 90 lbs * kg per pound
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
 
@@ -44,9 +44,9 @@ public final class Constants
   {
 
     // Joystick Deadband
-    public static final double LEFT_X_DEADBAND  = 0.01;
-    public static final double LEFT_Y_DEADBAND  = 0.01;
-    public static final double RIGHT_X_DEADBAND = 0.01;
+    public static final double LEFT_X_DEADBAND  = 0.15;
+    public static final double LEFT_Y_DEADBAND  = 0.15;
+    public static final double RIGHT_X_DEADBAND = 0.15;
     public static final double TURN_CONSTANT    = 6;
   }
 }
