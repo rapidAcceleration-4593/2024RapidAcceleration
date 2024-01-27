3034d5abb760090cd60aace763e72b0fb9e418de
rapidAcceleration-4593 <78278915+rapidAcceleration-4593@users.noreply.github.com>
2024-01-27 13:47:31 -0700

    strafe align works ?!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,11 +5,9 @@
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.AlignWithAprilTag;
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,14 +1,9 @@
 package frc.robot.subsystems;
 
-import java.util.function.DoubleSupplier;
-
 import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
@@ -29,13 +24,7 @@ public class Vision extends SubsystemBase {
 
         System.out.println(verticalOffset);
 
-
-
-        if (verticalOffset > 0) {
-             swerveSubsystem.drive(new Translation2d(0.0, verticalOffset), 0.0, true);
-        } else if (verticalOffset < 0) {
-             swerveSubsystem.drive(new Translation2d(0.0, -verticalOffset), 0.0, true);
-        }
+        swerveSubsystem.drive(new Translation2d(0.0, -verticalOffset * .05), 0.0, false);
     }
 
     public void periodic() {
