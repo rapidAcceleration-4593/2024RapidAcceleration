35672d3050f1b8d2e61014d90991a36f8520bbb5
rapidAcceleration-4593 <78278915+rapidAcceleration-4593@users.noreply.github.com>
2024-01-29 20:36:25 -0700

    ITS FOLLOWING OMG

--- src/main/java/frc/robot/commands/AlignWithAprilTag.java
+++ src/main/java/frc/robot/commands/AlignWithAprilTag.java
@@ -1,33 +1,46 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
 public class AlignWithAprilTag extends Command {
     
-    private final NetworkTable table;
     private final SwerveSubsystem swerve;
+    private final double kP = 0.09;
+    private final double kI = 0.0;
+    private final double kD = 0.0;
+    private PIDController pid = new PIDController(kP, kI, kD);
 
     public AlignWithAprilTag(SwerveSubsystem swerve) {
-        // m_vision = visionPassedIn;
-        // addRequirements(visionPassedIn);
-        
         this.swerve = swerve;
-
-        table = NetworkTableInstance.getDefault().getTable("limelight");
     }
 
     @Override
     public void execute() {
-        NetworkTableEntry ty = table.getEntry("ty");
-        double verticalOffset = ty.getDouble(0.0);
+        double verticalOffset = LimelightHelpers.getTY("");
+        double targetArea = LimelightHelpers.getTA("");
 
         System.out.println(verticalOffset);
 
-        swerve.drive(new Translation2d(0.0, -verticalOffset * 0.1), -verticalOffset * 0.1, false);
+        // if (verticalOffset < -2 || verticalOffset > 2) {
+        //     swerve.drive(new Translation2d(0.0, -verticalOffset * 0.09), -verticalOffset * 0.09, false);
+        // } else {
+        //     swerve.drive(new Translation2d(0.0, 0.0), 0.0, false);
+        // }
+
+        if (targetArea < 2 && targetArea > 0) {
+            swerve.drive(new Translation2d(-1, 0.0), 0.0, false);
+        } else {
+            swerve.drive(new Translation2d(0.0, 0.0), 0.0, false);
+        
+             if (verticalOffset < -2 || verticalOffset > 2) {
+                swerve.drive(new Translation2d(0.0, -verticalOffset * 0.09), -verticalOffset * 0.09, false);
+            } else {
+                swerve.drive(new Translation2d(0.0, 0.0), 0.0, false);
+        }
+        }
     }
 }
