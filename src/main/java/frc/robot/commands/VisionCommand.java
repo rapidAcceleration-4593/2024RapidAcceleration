3c050a9eea319c9a713f73cf6e8f12ee0eeade5d
Tyler Mueller <tylerjacobmueller08@gmail.com>
2024-01-31 16:08:06 -0700

    work please

--- src/main/java/frc/robot/commands/VisionCommand.java
+++ src/main/java/frc/robot/commands/VisionCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.LimelightHelpers;
@@ -10,40 +11,47 @@ public class VisionCommand extends Command {
 
     private final SwerveSubsystem swerve;
     private final PIDController rotationController;
+    private final PIDController translationController;
+
+    private static final int targetID = 1;
 
     public VisionCommand(SwerveSubsystem swerve) {
+        // Initialize Swerve
         this.swerve = swerve;
 
+        // Initalize Translation PID Controller
+        double kPTranslation = 0.0;
+        double kITranslation = 0.0;
+        double KDTranslation = 0.0;
+        translationController = new PIDController(kPTranslation, kITranslation, KDTranslation);
+        translationController.setSetpoint(0.0);
+        translationController.setTolerance(0.5);
+
+        // Initialize Rotation PID Controller
         double kPRotation = 0.15;
         double kIRotation = 0.01;
         double kDRotation = 0.02;
         rotationController = new PIDController(kPRotation, kIRotation, kDRotation);
         rotationController.setSetpoint(0.0);
-        // rotationController.setTolerance(3);
+        rotationController.setTolerance(0.5);
     }
 
     @Override
     public void execute() {
-        double horizontalOffset = LimelightHelpers.getTY("");
+        boolean hasTargets = LimelightHelpers.getTV("");
         double targetArea = LimelightHelpers.getTA("");
+        double horizontalOffset = LimelightHelpers.getTY("");
 
-        double kPTranslation = 1.0;
-        double kPRotation = 0.09;
-        
-        double rotateAdjust = rotationController.calculate(horizontalOffset);
+        // Check if the selected AprilTag ID is visible
+        if (hasTargets && LimelightHelpers.getFiducialID("") == targetID) {
+            double translationAdjustX = translationController.calculate(targetArea);
+            double translationAdjustY = translationController.calculate(rotationController.getPositionError());
+            double rotationAdjust = rotationController.calculate(-horizontalOffset);
 
-        swerve.drive(new Translation2d(0.0, 0.0), rotateAdjust, false);
-
-        // if (targetArea < 1.5 && targetArea > 0) {
-        //     swerve.drive(new Translation2d(-1 * kPTranslation, 0.0), 0.0, false);
-        // } else if (targetArea > 3) {
-        //     swerve.drive(new Translation2d(1 * kPTranslation, 0.0), 0.0, false);
-        // } else {
-        //     if (horizontalOffset <= -2 || horizontalOffset >= 2) {
-        //         swerve.drive(new Translation2d(0.0, -horizontalOffset * kPRotation), -horizontalOffset * kPRotation, false);
-        //     } else {
-        //         swerve.drive(new Translation2d(0.0, 0.0), 0.0, false);
-        //     }
-        // }
+            swerve.drive(new Translation2d(translationAdjustX, translationAdjustY), rotationAdjust, false);
+        } else {
+            // No AprilTag detected, stop
+            swerve.drive(new Translation2d(0.0, 0.0), 0.0, false);
+        }
     }
 }
