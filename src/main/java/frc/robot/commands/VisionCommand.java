34870e4b86d93431cd88f264207fafbe68710ac1
rapidAcceleration-4593 <78278915+rapidAcceleration-4593@users.noreply.github.com>
2024-01-31 20:28:40 -0700

    somewhat working idk fix it please for the love of god

--- src/main/java/frc/robot/commands/VisionCommand.java
+++ src/main/java/frc/robot/commands/VisionCommand.java
@@ -11,29 +11,38 @@ public class VisionCommand extends Command {
 
     private final SwerveSubsystem swerve;
     private final PIDController rotationController;
-    private final PIDController translationController;
+    private final PIDController translationControllerX;
+    private final PIDController translationControllerY;
 
-    private static final int targetID = 1;
+    private static final int targetID = 11;
 
     public VisionCommand(SwerveSubsystem swerve) {
-        // Initialize Swerve
+        // Initialize Swerv3
         this.swerve = swerve;
 
-        // Initalize Translation PID Controller
-        double kPTranslation = 0.0;
-        double kITranslation = 0.0;
-        double KDTranslation = 0.0;
-        translationController = new PIDController(kPTranslation, kITranslation, KDTranslation);
-        translationController.setSetpoint(0.0);
-        translationController.setTolerance(0.5);
+        // Initalize TranslationX PID Controller
+        double kPTranslationX = 1.8;
+        double kITranslationX = 0.03;
+        double KDTranslationX = 0.06;
+        translationControllerX = new PIDController(kPTranslationX, kITranslationX, KDTranslationX);
+        translationControllerX.setSetpoint(1.0);
+        translationControllerX.setTolerance(0.25);
+
+        // Initalize TranslationY PID Controller
+        double kPTranslationY = 0.06;
+        double kITranslationY = 0.0;
+        double KDTranslationY = 0.01;
+        translationControllerY = new PIDController(kPTranslationY, kITranslationY, KDTranslationY);
+        translationControllerY.setSetpoint(0.0);
+        translationControllerY.setTolerance(0.6);
 
         // Initialize Rotation PID Controller
-        double kPRotation = 0.15;
-        double kIRotation = 0.01;
-        double kDRotation = 0.02;
+        double kPRotation = 0.14;
+        double kIRotation = 0.001;
+        double kDRotation = 0.01;
         rotationController = new PIDController(kPRotation, kIRotation, kDRotation);
         rotationController.setSetpoint(0.0);
-        rotationController.setTolerance(0.5);
+        rotationController.setTolerance(0.7);
     }
 
     @Override
@@ -44,14 +53,16 @@ public class VisionCommand extends Command {
 
         // Check if the selected AprilTag ID is visible
         if (hasTargets && LimelightHelpers.getFiducialID("") == targetID) {
-            double translationAdjustX = translationController.calculate(targetArea);
-            double translationAdjustY = translationController.calculate(rotationController.getPositionError());
-            double rotationAdjust = rotationController.calculate(-horizontalOffset);
+            double translationAdjustX = translationControllerX.calculate(targetArea);
+            double translationAdjustY = translationControllerY.calculate(rotationController.getPositionError());
+            double rotationAdjust = rotationController.calculate(horizontalOffset);
 
-            swerve.drive(new Translation2d(translationAdjustX, translationAdjustY), rotationAdjust, false);
+            swerve.drive(new Translation2d(-translationAdjustX, -translationAdjustY), rotationAdjust, false);
         } else {
+
             // No AprilTag detected, stop
             swerve.drive(new Translation2d(0.0, 0.0), 0.0, false);
         }
+        
     }
 }
